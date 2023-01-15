499715804631fe6efc2c020508ba8b498f8e3132
Võ Nhật Trường <truongvn@vng.com.vn>
2023-01-15 12:10:18 +0700

    debug send mail

--- core/service/mail/index.ts
+++ core/service/mail/index.ts
@@ -1,6 +1,5 @@
-import nodemailer from "npm:nodemailer"
+import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-
 const env = config();
 
 interface informationMail{
@@ -10,30 +9,26 @@ interface informationMail{
     html: string,
 }
 
-const TRANSPORTER = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    secure: true,
-    auth: {
-        user: env.EMAIL_DOMAIN,
-        pass: env.EMAIL_APP_PASSWORD,
+const client = new SMTPClient({
+    connection: {
+        hostname: "smtp.gmail.com",
+        port: 465,
+        tls: true,
+        auth: {
+            username: env.EMAIL_DOMAIN,
+            password: env.EMAIL_APP_PASSWORD,
+        }
     }
 });
 
-
 export async function sendMail() {
-     TRANSPORTER.sendMail({
+    await client.send({
         from: env.EMAIL_DOMAIN,
         to: "truongvo.dev@gmail.com", // list of receivers
         subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
+         content: "Hello world?", // plain text body
         html: "<b>Hello world?</b>", // html body
-    },(error: any, info: any) => {
-         if (error) {
-             console.log(error);
-         } else {
-             console.log(`Email sent: ${info.response}`);
-         }
-     }
- );
+    });
+
+    return await client.close();
 }
