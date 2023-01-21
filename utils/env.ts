fd5279bdcda61d26063d0f2d271504147191441d
Võ Nhật Trường <truongvn@vng.com.vn>
2023-01-21 19:25:05 +0700

    debug

--- utils/env.ts
+++ utils/env.ts
@@ -4,6 +4,7 @@ export function loadENV() {
     try {
         config({ export: true });
     } catch (error) {
+        console.log(error)
         console.warn("Failed to read environment variables from \".env\" file,");
         console.info("are you running in a Deno Deploy environment? This is an expected error, so it works only with \"Deno.env\".");
     }
